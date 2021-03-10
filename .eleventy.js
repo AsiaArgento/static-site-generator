cf00afcde3a387b45c7815aafaae2cfbb7db1f7c
Matt M Moncada <misakamikotocsr@gmail.com>
2021-03-10 16:22:00 -0700

    added an api

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,8 @@ module.exports = function(config) {
   return {
     dir: {
       input: "src",
-      output: "dist"
+      output: "dist",
+      data: "_data"
     }
   };
 
---
+++ src/_data/news.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = async function() {
+  try {
+    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+//a3528a841e824800856fc9e628594de9
